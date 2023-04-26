49dbbf1878b5a46a9a2165ea282762624278ae41
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-04-26 23:18:32 +0200

    update

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.61.0"
+    }
+  }
+  backend "gcs" {
+    bucket = "parisaiacweu"
+    prefix = "enterprise-network/weu4"
+  }
+}
+
+provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+# install gcloud cli : https://cloud.google.com/sdk/docs/install
